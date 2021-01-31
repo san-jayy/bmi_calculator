5edacec9eff53f491b5cdabda7f7d6672c122969
Sanjay-2000 <sanjay.space.66@gmail.com>
2021-01-31 10:51:33 +0530

    created a seperate lib file for all constants

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kbottomcontainerheight = 75.0;
+const kbottomcontainercolor = Color(0xFF303346);
+const kactivecolor = Color(0xFFA7AAB3);
+const kinactivecardcolor = Color(0xFFD6D6D6);
+
+const klabeltextstyle = TextStyle(
+    fontSize: 21.0, color: Color(0xFF303346), fontWeight: FontWeight.bold);
+const knumberstyle = TextStyle(
+    color: kbottomcontainercolor, fontSize: 55, fontWeight: FontWeight.w900);
